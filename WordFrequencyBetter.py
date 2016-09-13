126b204e37c8beac9f490708d7de0b9a56be8e23
Ketaki Mulajkar <mulajkarketaki13@gmail.com>
2016-09-12 22:44:30 -0400

    Add files via upload

---
+++ WordFrequencyBetter.py
@@ -0,0 +1,22 @@
+from mrjob.job import MRJob
+import re
+
+WORD_REGEXP = re.compile(r"[\w+]")
+
+class MRWordFrequencyBetter(MRJob):
+    
+    def mapper(self, _, line):
+        words = WORD_REGEXP.findall(line)
+        for word in words:
+            word = unicode(word, "utf-8", errors="ignore") #avoids issues in mrjob 5.0
+            yield word.lower(), 1
+    
+    def reducer(self, key, values):
+        yield key, sum(values)
+        
+        
+if __name__ == '__main__':
+    MRWordFrequencyBetter.run()
+    
+    
+    
